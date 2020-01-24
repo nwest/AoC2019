d4b651ed12774cdfc571cf0463b46ded0e1900ac
Nate West <nwestr@gmail.com>
2020-01-24 16:12:39 -0500

    3a

--- 1.hs
+++ 1.hs
@@ -1,8 +1,14 @@
 {-# OPTIONS_GHC -Wall -Werror #-}
 {-# LANGUAGE ViewPatterns #-}
 
+import qualified Data.Text as T
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Data.List (foldl')
+
+
 input1 :: IO [Int]
-input1 = map read . lines <$> readFile "/home/nwest/Projects/AoC/1"
+input1 = map read . lines <$> readFile "./1"
 
 fuel :: Int -> Int
 fuel = (2 `subtract`) . (`quot` 3)
@@ -23,7 +29,7 @@ cleanCSV :: String -> [Int]
 cleanCSV xs = read . concat $ [ "[", xs, "]" ]
 
 input2 :: IO [Int]
-input2 =  cleanCSV . head . lines <$> readFile "/home/nwest/Projects/AoC/2"
+input2 =  cleanCSV . head . lines <$> readFile "./2"
 
 crunch2 :: (Int -> Int -> Int) -> [Int] -> [Int] -> [Int] --maybe use Vector
 crunch2 f [a, b, c] xs = let g x = head . drop x $ xs
@@ -60,4 +66,41 @@ num2b = do let possible = (,) <$> [0..99] <*> [0..99] -- [(x, y) | x <- [0..99],
 
 -------------------
 
+input3 :: IO [[WireRoute]]
+input3 =  map (map instr . splitOn ",") . lines <$> readFile "./3"
 
+splitOn :: String -> String -> [String]
+splitOn c = map T.unpack . T.splitOn (T.pack c) . T.pack
+
+type WireRoute = (Char, Int)
+type WireCoords = Set (Int, Int)
+
+instr :: String -> WireRoute
+instr (x:xs) = (x, read xs)
+instr []     = ('X', 0)
+
+route :: (Int, Int) -> WireRoute -> [(Int, Int)]
+route start (dir, moves) | dir == 'U' = f up
+                         | dir == 'D' = f down
+                         | dir == 'L' = f left
+                         | dir == 'R' = f right
+                         | otherwise = []
+                         where f g = start : reverse (g moves start)
+                               up    n (a, b) = foldl' (\xs x -> (a, b + x): xs) [] [1..n]
+                               down  n (a, b) = foldl' (\xs x -> (a, b - x): xs) [] [1..n]
+                               left  n (a, b) = foldl' (\xs x -> (a - x, b): xs) [] [1..n]
+                               right n (a, b) = foldl' (\xs x -> (a + x, b): xs) [] [1..n]
+
+wireCoords :: (Int, Int) -> [WireRoute] -> WireCoords
+wireCoords c = Set.fromList . foldl' (\xs x -> xs ++ route (last xs) x) [c]
+
+manhattan :: (Int, Int) -> (Int, Int) -> Int
+manhattan (a, b) (c, d) = abs(a - c) + abs(b - d)
+
+num3 :: IO Int
+num3 = do
+       wireA <- wireCoords (0,0) . head <$> input3
+       wireB <- wireCoords (0,0) . last <$> input3
+       let intersections = Set.delete (0,0) (Set.intersection wireA wireB)
+           distances = Set.map (manhattan (0,0)) intersections
+       return $ Set.findMin distances
